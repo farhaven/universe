2e468e1124193460accadf365145bf1b408fc5b0
Gregor Best <gbe@unobtanium.de>
2016-01-28 15:36:30 +0100

    Missed in second-to-last commit

--- vector/vector.go
+++ vector/vector.go
@@ -5,7 +5,7 @@ import "math"
 type V3 struct {
 	X, Y, Z float64
 }
-func (v *V3) Length() float64 {
+func (v V3) Length() float64 {
 	return math.Sqrt(v.X * v.X + v.Y * v.Y + v.Z * v.Z)
 }
 func (v V3) Dot(o V3) float64 {
@@ -30,6 +30,9 @@ func (v V3) Add(o V3) V3 {
 }
 func (v V3) Scaled(n float64) V3 {
 	return V3{v.X * n, v.Y * n, v.Z * n}
+}
+func (v V3) Distance(o V3) float64 {
+	return v.Sub(o).Length()
 }
 
 type Plane [2]V3 // Normal, Point on plane
